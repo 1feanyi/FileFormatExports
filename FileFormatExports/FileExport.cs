b7c6834ad60fa5959d99f2faefa401d19b5b74e7
Ifeanyi Igbo <1feanyi@users.noreply.github.com>
2020-01-02 14:34:51 -0500

    Prototype commit

---
+++ FileFormatExports/FileExport.cs
@@ -0,0 +1,52 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace FileFormatExports
+{
+    public class FileExport
+    {
+        public static Task CSV<T>(List<T> input)
+        {
+            string filepath = DateTime.UtcNow.ToFileTimeUtc() + ".csv";
+
+            using (var writer = new StreamWriter(filepath))
+            using (var csv = new CsvWriter(writer))
+            {
+                csv.WriteRecords(input);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public static Task JSON<T>(List<T> input)
+        {
+            string filepath = DateTime.UtcNow.ToFileTimeUtc() + ".json";
+            using (StreamWriter file = File.CreateText(filepath))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, input);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public static Task XML<T>(List<T> input)
+        {
+            string filePath = DateTime.UtcNow.ToFileTimeUtc() + ".xml";
+
+            XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+            using (TextWriter writer = new StreamWriter(filePath))
+            {
+                serializer.Serialize(writer, input);
+                writer.Close();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ FileFormatExports/Program.cs
@@ -0,0 +1,29 @@
+﻿using FileFormatExports;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleSandbox
+{
+    public class Program
+    {
+        public class Person
+        {
+            public int Id { get; set; }
+            public string SSN { get; set; }
+            public string Name { get; set; }
+            public DateTime DoB { get; set; }
+        }
+
+        public static void Main(string[] args)
+        {
+            var _data = new List<Person>();
+            _data.Add(new Person() { Id = 1, SSN = "136-568-5787", Name = "John Smith", DoB = DateTime.Today });
+            _data.Add(new Person() { Id = 2, SSN = "111-525-6545", Name = "Mary Jones", DoB = DateTime.Today.AddMonths(-36) });
+            _data.Add(new Person() { Id = 3, SSN = "151-125-2525", Name = "Dick Wolf", DoB = DateTime.Today.AddMonths(-24) });
+
+            FileExport.CSV(_data);
+            FileExport.JSON(_data);
+            FileExport.XML(_data);
+        }
+    }
+}
